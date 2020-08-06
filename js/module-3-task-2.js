18e5a9e08c9bc7c6057a52e49c950895b3805675
Dmytro Petrenko <blinker182ca@gmail.com>
2020-08-06 13:18:36 +0300

    done 2nd task

--- js/module-3-task-2.js
+++ js/module-3-task-2.js
@@ -1,36 +1,21 @@
 console.log('--- Task 2 ---');
 
-// const calculateEngravingPrice = (message = '', pricePerWord = 0) => {
-// 	let wordsCount = message.split(' ').length;
-// 	let result = wordsCount * pricePerWord;
+const countProps = function(obj) {
+	// Write code under this line
+	return Object.keys(obj).length;
+};
 
-// 	console.log(wordsCount);
-// 	// console.log(result);
-// 	// console.log(pricePerWord);
+console.log(countProps({})); // 0
 
-// 	return result;
-// };
+console.log(countProps({ a: 1, b: 1 })); // 2
 
-const calculateEngravingPrice = (message = '', pricePerWord = 0) => message.split(' ').length * pricePerWord;
-
-console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
-
-console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
-
-console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
-
-console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
-
-console.log(calculateEngravingPrice('Uno', 100)); // 100
-
-// console.log(pricePerWord);
+console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
 
 /* 
-Задача 2-2
-Подсчет стоимости гравировки украшений
-Напиши скрипт подсчета стоимости гравировки украшений.
-Для этого создай функцию
-calculateEngravingPrice(message = "", pricePerWord = 0) принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
 
-Для решения этой задачи не используйте циклы. Т.е. никаких for, while, do while, for of, for in, forEach или функциональных методов.
+Напиши функцию countProps(obj),
+которая возвращает число - количество свойств в объекте.
+
+Циклы не должны использоваться
+
 */
